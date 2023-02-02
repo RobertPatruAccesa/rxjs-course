75d00cf14ac975aafe9532c6759730a42f6f06db
RobertPatruAccesa <113914427+RobertPatruAccesa@users.noreply.github.com>
2023-02-02 15:43:59 +0200

    Created an observable. I used map operator

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { interval, timer, fromEvent, Observable, noop } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { createHttpObservable } from '../common/util';
 
 @Component({
-  selector: 'about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+	selector: 'about',
+	templateUrl: './about.component.html',
+	styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+	constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+	ngOnInit() {
+		
+	}
 }
--- src/app/common/util.ts
+++ src/app/common/util.ts
@@ -1,2 +1,19 @@
+import { Observable } from "rxjs";
 
+export function createHttpObservable(url: string) {
+	return Observable.create(observer => {
+		fetch(url)
+			.then(respose => {
+				return respose.json();
+			})
+			.then(body => {
+				observer.next(body);
+
+				observer.complete();
+			})
+			.catch(err => {
+				observer.error(err);
+			});
+	});
+}
 
