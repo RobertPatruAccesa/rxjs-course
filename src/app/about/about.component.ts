2c5235864c3bb56d7c3d18c50acd28a8abd11eb1
RobertPatruAccesa <113914427+RobertPatruAccesa@users.noreply.github.com>
2023-02-07 17:52:59 +0200

    i

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,32 +1,23 @@
-import { Component, OnInit, ViewEncapsulation } from "@angular/core";
-import {
-  concat,
-  fromEvent,
-  interval,
-  noop,
-  observable,
-  Observable,
-  of,
-  timer,
-  merge,
-  Subject,
-} from "rxjs";
-import { delayWhen, filter, map, take, timeout } from "rxjs/operators";
-import { createHttpObservable } from "../common/util";
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { concat, fromEvent, interval, noop, observable, Observable, of, timer, merge, Subject } from 'rxjs';
+import { delayWhen, filter, map, take, timeout } from 'rxjs/operators';
+import { createHttpObservable } from '../common/util';
 
 @Component({
-  selector: "about",
-  templateUrl: "./about.component.html",
-  styleUrls: ["./about.component.css"],
+	selector: 'about',
+	templateUrl: './about.component.html',
+	styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-  ngOnInit() {
-    const subject = new Subject();
+	ngOnInit() {
+		const subject = new Subject();
+		const serier$ = subject.asObservable();
 
-    subject.next(1);
-    subject.next(2);
-    subject.next(3);
+		serier$.subscribe(res => console.log(res));
 
-    const serier$ = subject.asObservable();
-  }
+		subject.next(1);
+		subject.next(2);
+		subject.next(3);
+		subject.complete();
+	}
 }
