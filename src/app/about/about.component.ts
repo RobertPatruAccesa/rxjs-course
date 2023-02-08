3edaeaa50cf249ef2af073cc116e710561f8e100
RobertPatruAccesa <113914427+RobertPatruAccesa@users.noreply.github.com>
2023-02-08 09:54:02 +0200

    h

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,19 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { concat, fromEvent, interval, noop, observable, Observable, of, timer, merge, Subject } from 'rxjs';
+import {
+	concat,
+	fromEvent,
+	interval,
+	noop,
+	observable,
+	Observable,
+	of,
+	timer,
+	merge,
+	Subject,
+	BehaviorSubject,
+	AsyncSubject,
+    ReplaySubject
+} from 'rxjs';
 import { delayWhen, filter, map, take, timeout } from 'rxjs/operators';
 import { createHttpObservable } from '../common/util';
 
@@ -10,14 +24,6 @@ import { createHttpObservable } from '../common/util';
 })
 export class AboutComponent implements OnInit {
 	ngOnInit() {
-		const subject = new Subject();
-		const serier$ = subject.asObservable();
-
-		serier$.subscribe(res => console.log(res));
-
-		subject.next(1);
-		subject.next(2);
-		subject.next(3);
-		subject.complete();
+		
 	}
 }
